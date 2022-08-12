a6faedb96516ebad5447ea547be8b3d58651d4fb
guodfoo <gdfoo94@gmail.com>
2022-08-12 16:46:48 +0800

    Added API_KEY to RequestEntity

--- src/main/java/com/assessment/ssf/Service/NewsService.java
+++ src/main/java/com/assessment/ssf/Service/NewsService.java
@@ -3,6 +3,7 @@ package com.assessment.ssf.Service;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,15 +13,19 @@ import com.assessment.ssf.Repository.NewsRepository;
 @Service
 public class NewsService {
 
-    private final String CRYPTO_NEWS_URL = "https://min-api.cryptocompare.com/data/v2/news/?lang=EN";
+    private static final String CRYPTO_NEWS_URL = "https://min-api.cryptocompare.com/data/v2/news/?lang=EN";
+    private static final String CRYPTO_API_KEY = System.getenv("CRYPTO_API_KEY");
 
     @Autowired
     private RestTemplate restTemplate;
     @Autowired
     private NewsRepository repo;
 
+
     public List<Article> getArticles(){
-        ResponseEntity<String> respEntity = restTemplate.getForEntity(CRYPTO_NEWS_URL, String.class);
+        RequestEntity<Void> reqEntity = RequestEntity.get(CRYPTO_NEWS_URL)
+                        .header("authorization", "Apikey " + CRYPTO_API_KEY).build();
+        ResponseEntity<String> respEntity = restTemplate.exchange(reqEntity, String.class);
         String payload = respEntity.getBody();
         Article.createArticleList(payload);
         return Article.listOfArticles;
